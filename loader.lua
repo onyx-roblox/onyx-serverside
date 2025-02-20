0684036e063a1bc7683d02a3023d6443b1155878
onyx-roblox <zeidgazibegovic737@gmail.com>
2025-02-20 19:58:52 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,10 +1,7 @@
 local module = {}
 
 function module.load(plr)
-	local code1 = game:GetService("HttpService"):GetAsync("https://github.com/Roblox/rodux/something.lua")
-  local gui_script = Instance.new("Script")
-  gui_script.Parent = plr.PlayerGui
-  gui_script.Source = code
+    print("test")
 end
 
 return module
